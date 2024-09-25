9ed692f89022413f6c5817c77e8a22869c4486cb
zwforum <782552186@qq.com>
2024-09-26 01:47:25 +0800

    fix fileserver

--- plugins/fileserv/main.go
+++ plugins/fileserv/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var fs http.FileSystem = http.Dir("/opt/imashen/zerotier-webui/etc/myfs")
+	var fs http.FileSystem = http.Dir("/www/zerotier-webui/etc/myfs")
 	var fsHandler = http.FileServer(fs)
 	log.Fatal(http.ListenAndServe(":3180", fsHandler))
 }
